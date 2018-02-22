c47cdff0eb2da4794144aef94423e5e6003827dd
ACinch <30296809+ACinch@users.noreply.github.com>
2018-02-22 13:57:52 -0800

    Added label definitions
    Added new label type definitions for Avery Label Types: 5160, 5162, 5260, 5262, 5520, 5522, 5660, 5810, 5960, 5962, 5970, 5971, 5972, 5979, 5980, 8160, 8162, 8252, 8460, 8462, 8660, 8662, and 8810

---
+++ Labels/A4Labels/Avery/L5160.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpPDFLabel.Labels.A4Labels.Avery
+{
+    /// <summary>
+    /// Dimensions: 66.675mm x 25.4
+    /// Per Sheet: 30
+    /// </summary>
+    public class L5160 : Label
+    {
+        public L5160()
+        {
+            _Width = 66.675;
+            _Height = 25.4;
+            _HorizontalGapWidth = 3.556;
+            _VerticalGapHeight = 0;
+
+            _PageMarginTop = 12.7;
+            _PageMarginBottom = 12.7;
+            _PageMarginLeft = 5.58165;
+            _PageMarginRight = 5.58165;
+
+            PageSize = Enums.PageSize.A4;
+            LabelsPerRow = 3;
+            LabelRowsPerPage = 10;
+        }
+    }
+
+    public class L5260 : L5160 { }
+    public class L5520 : L5160 { }
+    public class L5660 : L5160 { }
+    public class L5810 : L5160 { }
+    public class L5960 : L5160 { }
+    public class L5970 : L5160 { }
+    public class L5971 : L5160 { }
+    public class L5972 : L5160 { }
+    public class L5979 : L5160 { }
+    public class L5980 : L5160 { }
+    public class L8160 : L5160 { }
+    public class L8460 : L5160 { }
+    public class L8660 : L5160 { }
+    public class L8810 : L5160 { }
+}
---
+++ Labels/A4Labels/Avery/L5162.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpPDFLabel.Labels.A4Labels.Avery
+{
+    /// <summary>
+    /// Dimensions: 66.675mm x 25.4
+    /// Per Sheet: 30
+    /// </summary>
+    public class L5162 : Label
+    {
+        /// <summary>
+        /// Dimensions: 101.6mm x 33.782mm 
+        /// Per Sheet: 14
+        /// </summary>
+        public L5162()
+        {
+            _Width = 101.6;
+            _Height = 33.782;
+            _HorizontalGapWidth = 3.96875;
+            _VerticalGapHeight = 0;
+
+            _PageMarginTop = 22.352;
+            _PageMarginBottom = 22.352;
+            _PageMarginLeft = 3.8735;
+            _PageMarginRight = 3.8735;
+
+            PageSize = Enums.PageSize.A4;
+            LabelsPerRow = 2;
+            LabelRowsPerPage = 7;
+        }
+    }
+
+    public class L5262 : L5162 { }
+    public class L5522 : L5162 { }
+    public class L5962 : L5162 { }
+    public class L8162 : L5162 { }
+    public class L8252 : L5162 { }
+    public class L8462 : L5162 { }
+    public class L8662 : L5162 { }
+}
