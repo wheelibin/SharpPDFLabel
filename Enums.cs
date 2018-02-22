e6ea16ad35edbde13572ac9f3cddfc958b9af277
ACinch <30296809+ACinch@users.noreply.github.com>
2018-02-22 13:56:47 -0800

    Label Definitions enum and helper
    Added enum of available Label definitions
    Added Helper class that maps enum to label definitions

--- Enums.cs
+++ Enums.cs
@@ -33,7 +33,39 @@ namespace SharpPDFLabel
             UNDERLINE = 4,
         }
 
-        
+        public enum FontFamily
+        {
+            Arial
+        }
 
+        public enum LabelStyle
+        {
+            L5160,
+            L5162,
+            L5260,
+            L5262,
+            L5520,
+            L5522,
+            L5660,
+            L5810,
+            L5960,
+            L5962,
+            L5970,
+            L5971,
+            L5972,
+            L5979,
+            L5980,
+            L7160,
+            L7654,
+            L7656,
+            L8160,
+            L8162,
+            L8252,
+            L8460,
+            L8462,
+            L8660,
+            L8662,
+            L8810
+        }
     }
 }
