7f1ebf95f00100410a36db1fc37bf868af01f2a4
nicholas gudger <nick.gudger@live.co.uk>
2020-02-05 22:18:50 +0000

    Added L7165 Parcel label class

---
+++ Labels/A4Labels/Avery/L7165.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpPDFLabel.Labels.A4Labels.Avery
+{
+    public class L7165 : Label
+    {
+        public L7165()
+        {
+            _Width = 100;
+            _Height = 67;
+            _HorizontalGapWidth = 2.5;
+            _VerticalGapHeight = 0;
+
+            _PageMarginTop = 13.1;
+            _PageMarginBottom = 13.1;
+            _PageMarginLeft = 4.65;
+            _PageMarginRight = 4.65;
+
+            PageSize = Enums.PageSize.A4;
+            LabelsPerRow = 2;
+            LabelRowsPerPage = 4;
+        }
+    }
+}
